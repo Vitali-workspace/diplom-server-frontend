5f42f809e286fc8ab0abcef3db3c8e43909f5750
vitali <vitali.slepnyov@gmail.com>
2022-12-30 23:23:05 +0300

    feat: добавить запросы MoviesApi

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,27 @@
+const { API_MOVIES_URL } = require('../utils/constants');
+
+class MoviesApi {
+  constructor(url) {
+    this._moviesUrl = url;
+    this._headers = { 'Content-type': 'application/json' };
+  }
+
+  _checkError(res) {
+    if (!res.ok) {
+      return Promise.reject(`произошла ошибка: ${res.status}`)
+    }
+    return res.json();
+  }
+
+  getMoviesList() {
+    return fetch(this._moviesUrl, {
+      method: 'GET',
+      headers: this._headers,
+    }).then(this._checkError);
+  }
+
+}
+
+const requestMoviesApi = new MoviesApi({ API_MOVIES_URL });
+
+export default requestMoviesApi;
