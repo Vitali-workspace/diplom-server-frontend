f4807e8c0faf56deebc8d87b8a9fac5bb4af1716
vitali <vitali.slepnyov@gmail.com>
2023-01-02 23:22:37 +0300

    feat: добавить валидацию в Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,22 +9,16 @@ import './Login.css';
 function Login({ onLogin }) {
 
   const { isErrors, isValues, isValid, handleChangeInput } = useFormValidator();
-  const navigation = useNavigate();
-
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-
-  // React.useEffect(() => {
-  //   setEmail('');
-  //   setPassword('');
-  // }, []);
+  //  const navigation = useNavigate();
 
   function submitForm(evt) {
-    evt.preventDefault();
-    onLogin({
-      'email': isValues.email,
-      'password': isValues.password,
-    });
+    if (isValid) {
+      evt.preventDefault();
+      onLogin({
+        'email': isValues.email,
+        'password': isValues.password,
+      });
+    }
   }
 
   return (
@@ -42,6 +36,7 @@ function Login({ onLogin }) {
             onChange={handleChangeInput}
             required
           />
+          <span className='login__error'>{isErrors.email ? 'Некорректный email' : ''}</span>
 
           <p className='login__text-input'>Пароль</p>
           <input
@@ -51,7 +46,11 @@ function Login({ onLogin }) {
             value={isValues.password || ''}
             onChange={handleChangeInput}
             required
+            minLength={5}
           />
+          <span className='login__error login__error_position'>
+            {isErrors.password ? 'Некорректный пароль' : ''}
+          </span>
         </fieldset>
 
         <button className='login__button' type='submit'>Войти</button>
