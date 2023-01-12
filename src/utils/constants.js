c87ecdce7857404a53356af2038568d54137de52
vitali <vitali.slepnyov@gmail.com>
2023-01-12 06:50:25 +0300

    refactor: изменить константы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_MAIN_URL = 'http://localhost:3000';
-// export const API_MAIN_URL = 'http://api.diplom.vitali.nomoredomains.club';
+// export const API_MAIN_URL = 'http://localhost:3000';
+export const API_MAIN_URL = 'http://api.diplom.vitali.nomoredomains.club';
 export const API_MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
